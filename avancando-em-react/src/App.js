73b0c7606e3afb8cd02759ee6b83849bf6ac081a
Marcelo Nunes <104375874+marcelonuness@users.noreply.github.com>
2022-09-19 18:01:38 -0300

    pt 2

--- avancando-em-react/src/App.js
+++ avancando-em-react/src/App.js
@@ -1,6 +1,6 @@
 //components
-import ManageData from "./components/ManageData.js"
-
+//import ManageData from "./components/ManageData.js"
+import ListRender from "./components/ListRender.js"
 
 //styles
 import './App.css';
@@ -13,7 +13,7 @@ function App() {
 			<div>
 				<img src="https://a.cdn-hotels.com/gdcs/production101/d154/ee893f00-c31d-11e8-9739-0242ac110006.jpg?impolicy=fcrop&w=800&h=533&q=medium" alt="New York city" />
 			</div>
-			<ManageData></ManageData>
+			<ListRender></ListRender>
 		</div>
 	);
 }
--- avancando-em-react/src/components/ListRender.js
+++ avancando-em-react/src/components/ListRender.js
@@ -1,7 +1,36 @@
+import {useState} from "react"
+
 const ListRender = () => {
+	const [list] = useState(["Marcelo", "José", "Caio", "Werr"])
+	
+	const [users, setUsers] = useState([
+		{id: 1, nome: "Marcelo", idade: 44},
+		{id: 2, nome: "vanessa", idade: 31},
+		{id: 3, nome: "rodrigo", idade: 12},		
+	])
+
+	const deleteRandom = () => {
+		const randomNumber = Math.floor(Math.random() * 4)
+
+		setUsers((prevUsers) => {
+			return prevUsers.filter((user) => randomNumber !== user.id)
+		})
+	}
+
 	return (
+				
 		<div>
-		
+			<ul>
+				{list.map((item, i)=>(
+					<li key={i}>{item}</li>
+				))}
+			</ul>
+			<ul>
+				{users.map((user)=>(
+					<li key={user.id}>{user.nome} - {user.idade}</li>
+				))}
+			</ul>
+			<button onClick={deleteRandom}>Delete random user!</button>
 		</div>
 	)
 }
